1dbbe713e4fdc9eaf1e4b2b42a58b19111eae3bf
Buff <marcotundo@gmail.com>
2025-06-27 07:45:36 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -2,9 +2,5 @@
   deps = [
     pkgs.nodejs_20
     pkgs.npm
-    pkgs.nodePackages.typescript
-    pkgs.nodePackages.typescript-language-server
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.eslint
   ];
 } 
